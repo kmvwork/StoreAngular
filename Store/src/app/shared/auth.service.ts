41b678bad701116a394eb0a187033c2137805ff0
Maksim <maksimus-94@tut.by>
2020-11-23 15:53:29 +0300

    save token

--- Store/src/app/shared/auth.service.ts
+++ Store/src/app/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment.prod";
+import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,40 @@ export class AuthService {
   constructor(private http: HttpClient) {
   }
 
+
+
   login(user) {
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      )
+  }
+
+  private setToken(response) {
+    if (response) {
+      const expData = new Date(new Date().getTime() + +response.expiresIn * 1000)
+      localStorage.setItem('fb-token-exp', expData.toString())
+      localStorage.setItem('fb-token', response.idToken)
+    } else {
+      localStorage.clear()
+    }
+  }
+
+  get token() {
+    const expDate = new Date(localStorage.getItem('fb-token-exp'))
+    if (new Date > expDate) {
+      this.logout()
+      return null
+    } else {
+      return localStorage.getItem('fb-token')
+    }
+  }
+
+  logout() {
+    this.setToken(null)
+  }
+
+  isAuthenticated() {
+    return !!this.token
   }
 }
